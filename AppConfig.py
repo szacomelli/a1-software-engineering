3c3030781bbcbf87b59e6d37a94d37d2c2d8bef0
Sílvio E. S. Zacomelli <zacomellisilvio@gmail.com>
2026-09-22 19:20:37 -0300

    "fixing" __init__ overwriting attributes

--- AppConfig.py
+++ AppConfig.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 
+
 class AppConfig:
     environment = None
     currency = None
     debug = None
     __instance = None
+    __created = False
 
     def __new__(cls, *args, **kargs):
         if cls.__instance == None:
             cls.__instance = super().__new__(cls)
         return cls.__instance
 
-    def __init__(self, environment, currency, debug):
+    def __init__(self, environment=None, currency=None, debug=False):
+        if self.__created:
+            return
         self.environment = environment
         self.currency = currency
         self.debug = debug
+        self.__created = True
 
 
 config = AppConfig("production", "BRL", False)
