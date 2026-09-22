b226ccf8cd1df044824e6348479dd81aa7536f26
Sílvio E. S. Zacomelli <zacomellisilvio@gmail.com>
2026-09-22 19:26:39 -0300

    adding required tests

--- testsAppConfig.py
+++ testsAppConfig.py
@@ -2,6 +2,18 @@
 
 import AppConfig as ac
 
-obj = ac.AppConfig()
+obj1 = ac.AppConfig()
+obj2 = ac.AppConfig("asdasd", "USD", True)
 
-print(obj.currency)
+# testing if they point to the same object
+print(obj1 == obj2)
+
+# testing if __init__ of obj2 didnt overwrite
+# the obj1 attributes
+print(obj2.environment)
+print(obj2.currency)
+print(obj2.debug)
+
+# testing if obj2 "follow" changes made by obj1
+obj1.currency = "Mangos"
+print(obj2.currency)
