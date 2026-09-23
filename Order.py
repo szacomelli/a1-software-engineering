df00be9229404cf032f7f5fac3ff51db77b7d897
JDomingueti <jean.domingueti@gmail.com>
2026-09-23 15:24:15 -0300

    Implementação da questão 2

---
+++ Order.py
@@ -0,0 +1,132 @@
+class Product():
+  def __init__(self, name: str, price: float, quantity: int = 1):
+    self.name = name
+    self.price = price
+    self.quantity = quantity
+
+
+  def __str__(self):
+    return f"{self.name}; Quantity: {self.quantity}; Price: {self.price}"
+
+
+class Client():
+  def __init__(self, name: str):
+    self.name = name
+
+  def __str__(self):
+    return self.name
+
+
+class Order():
+  def __init__(self, client: Client):
+    self.client = client
+    self.coupon = None
+    self.address = None
+    self.pay_method = None
+    self.observation = None
+    self.products = []
+
+
+  def total(self) -> float:
+    total_value = 0
+    for product in self.products:
+      total_value += product.price * product.quantity
+
+    return total_value
+
+
+  def __str__(self):
+    prods = "".join(f" - {idx+1}: {product}\n" for idx, product in enumerate(self.products))
+    text = [f"Client name: {self.client or 'None'}\n",
+      f"Coupon used: {self.coupon} \n" if self.coupon else "",
+      f"Address inputed: {self.address or 'Not specified'}\n" if self.address else "",
+      f"Payment method: {self.pay_method or 'Not specified'}\n" if self.pay_method else "",
+      f"Obs.: {self.observation or 'None'}\n" if self.observation else "",
+      f"Products:\n{prods}"]
+
+    return "".join(txt for txt in text)
+
+
+class OrderBuilder():
+  def __init__(self, order = Order):
+    self.order = order(None)
+
+
+  def set_client(self, client):
+    self.order.client = client
+    return self
+
+
+  def set_coupon(self, coupon):
+    self.order.coupon = coupon
+    return self
+
+
+  def set_address(self, address):
+    self.order.address = address
+    return self
+
+
+  def set_pay_method(self, pay_method):
+    self.order.pay_method = pay_method
+    return self
+
+
+  def set_observation(self, observation):
+    self.order.observation = observation
+    return self
+
+
+  def add_product(self, product):
+    for old_product in self.order.products:
+      if old_product.name == product.name:
+        old_product.quantiy += product.quantity
+        return self
+      
+    self.order.products.append(product)
+    return self
+
+  def add_product_by_name(self, name: str, price: float, quantity: int = 1):
+    for old_product in self.order.products:
+      if old_product.name == name:
+        old_product.quantity += quantity
+        return self
+      
+    product = Product(name, price, quantity)
+    self.order.products.append(product)
+    return self
+
+
+  def build(self):
+    if not self.order.client:
+      raise ValueError("You must set a client for the order")
+    return self.order
+
+
+if __name__ == "__main__":
+  print("Rodando main...\n")
+
+  print("Definindo produtos\n")
+  maca = Product("Maçã", 1.00, 4)
+  print(maca)
+  macarrao = Product("Macarrão", 3.99, 6)
+  print(macarrao)
+
+  print("Definindo cliente\n")
+  cliente = Client("Ada Lovelace")
+  print(cliente)
+
+  print("Construindo pedido\n")
+  order = (OrderBuilder(Order)
+             .set_client(cliente)
+             .set_address("Rua Voluntários da Pátria")
+             .set_coupon("OMELHORCUPOM")
+             .set_pay_method("Pix")
+             .set_observation("Pedido deve ser entregue com urgência")
+             .add_product(maca)
+             .add_product(macarrao)
+             .add_product_by_name("Pera", 0.80, 3)
+             .build())
+
+  print(order)
+  print(f"{order.total():.2f}")
