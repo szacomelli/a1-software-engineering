bf27908d4306d947688521b5c7fc6ee26de40ce0
Sílvio E. S. Zacomelli <zacomellisilvio@gmail.com>
2026-09-23 15:00:52 -0300

    first definitions of Payment and PaymentProcessor along with concrete classes that implement behavior

---
+++ Payment.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+
+class Payment(ABC):
+    @abstractmethod
+    def pay(self, amount):
+        pass
+
+class PaymentProcessor(ABC):
+    @abstractmethod
+    def create_payment(self):
+        pass
+
+    def process_order(self, order):
+        payment = self.create_payment()
+        payment.pay(order.total())
+
+
+class PixPayment(Payment):
+    def pay(self, amount):
+        print(f"Pagou-se R${amount} com PIX")
+
+class CreditCardPayment(Payment):
+    def pay(self, amount):
+        print(f"Pagou-se R${amount} com cartão de crédito")
+
+class BoletoPayment(Payment):
+    def pay(self, amount):
+        print(f"Pagou-se R${amount} com boleto")
+
+
+class PixProcessor(PaymentProcessor):
+    def create_payment(self):
+        return PixPayment()
+
+class CreditCardProcessor(PaymentProcessor):
+    def create_payment(self):
+        return CreditCardPayment()
+
+class BoletoProcessor(PaymentProcessor):
+    def create_payment(self):
+        return BoletoPayment()
+
+class MockOrder:
+    def total(self):
+        return 10
